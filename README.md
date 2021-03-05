a63d4fd437f570d59f1c0748e76564279f6cf97f
mrjones-plip <mrjones-plip@plip.com>
2021-03-05 09:05:21 -0800

    re-deploy correct version

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Keys-To-The-Tunnel
-
+#  Keys-To-The-Tunnel
  
 ## Intro
 
@@ -12,7 +11,7 @@ The script will:
 1. Create an SSL certificate with Let's Encrypt for `GH-USERNAME.domain.com`
 1. Put instructions to use the SSH tunnels at `domain.com`
 
-Keys-To-The-Tunnel is named after the fact that it uses SSH tunnels and the keys for this are pivital to why it exists: easily provision accounts from GH users based off their SSH keys.
+Keys-To-The-Tunnel is named after the fact that it uses SSH tunnels and the keys for this are pivotal to why it exists: easily provision accounts from GH users based off their SSH keys.
 
 This script is [hosted on GitHub](https://github.com/mrjones-plip/mrjones-medic-scratch/tree/main/SshTunnelServer).
 
@@ -25,12 +24,101 @@ Right now this very narrowly scoped, so requirements are:
 1. a wildcard CNAME entry pointing to the A record
 1. SSH server [locked to keys only](https://www.linuxbabe.com/linux-server/setup-passwordless-ssh-login) (optional, but is very good idea)
 
-Development was done locally and then in Digital Ocean.
+Development was done locally in lxd containers and then in Digital Ocean.
 
-## Running
+## Installing
 
 1. SSH as root to your Ubuntu server with public IP
 1. clone this repo with `git clone https://github.com/mrjones-plip/mrjones-medic-scratch`
-1. cd into repo and add create `user.txt` with your github users, one per line
+1. `cd` into repo and add create `user.txt` with your github users, one per line
 1. run the install script with `./installTunnelServer.sh DOMAIN.COM EMAIL` replacing `DOMAIN.COM` with your real domain from step 3 in Requirements and replacing `EMAIL` with your email which will be used to agree to Let's Encrypt TOS and to get notifications about expiring certs.
 1. Send users the URL `DOMAIN.COM` which now lists how to use the server
+
+## Using
+
+Users should go to the website you provisioned and use the list of port/URL combos and instructions there. After that, any client that can use SSH should Just Work™.  This has been tested on Ubuntu and MacOS.
+
+### SSH Call
+
+The structure of the SSH call to set up the tunnel is:
+
+`ssh -T -R PORT-FROM-SERVER:127.0.0.1:PORT-ON-LOCALHOST GH-HANDLE@DOMAIN`
+
+### Example 1
+
+Assuming:
+
+* user `alligator-lovely`
+* domain `tunnel.domain.com` 
+* port: `4555`
+
+If a user has a server running on `http://localhost` (implicitly port 80), they would run:
+
+`ssh -T -R 4555:127.0.0.1:80 alligator-lovely@tunnel.domain.com`
+
+And then in a browser they could go to `https://alligator-lovely.tunnel.domain.com`.
+
+### Example 2
+
+Still assuming:
+
+* user `alligator-lovely`
+* domain `tunnel.domain.com`
+* port: `4555`
+
+If a user has a server running on `https://localhost:1234`, they would run:
+
+`ssh -T -R 4555:127.0.0.1:1234 alligator-lovely@tunnel.domain.com`
+
+And then in a browser they could go to `https://alligator-lovely-ssl.tunnel.domain.com` - note extra `-ssl` in URL! This ensures the proxy server speaks SSL to your localhost.
+
+## FAQ
+
+* **Q:** Wait...why not ansible, saltstack etc.?
+  
+  **A:** It started as "just a quick bash script" and then spiraled out of control from there.  Sorry!
+  
+
+* **Q:** How come I'm getting 400 errors in Apache `The plain http request was sent to https port`?
+  
+  **A:** A user has created an SSH tunnel using the non-ssl vhost in the top group of port/URL sets which points to a web server running SSL on localhost.  Have them use the `-ssl` vhost listed in the bottom group of port/URL sets.  The reason is that the Apache vhost has a hard coded proxy of either `ProxyPass / http://localhost:PORT/` or `ProxyPass / https://localhost:PORT/`, it can't be both.  
+  
+
+* **Q:** Why not use [ngrok](https://ngrok.com/), [pagekite](https://pagekite.net/), [localtunnel](https://github.com/localtunnel/localtunnel) or InsertSolutionHere instead?
+  
+  **A:** You totally can!  These are much more full featured and are much easier to use. [localtunnel](https://github.com/localtunnel/localtunnel)  may be of particular interest as it has a self hosted option.  Conversely, they cost more money this this script.  It's estimated a $5/mo VPS could support dozzens of users. This solution also offers authentication in the way of SSH keys. Finally, this script makes it trivially easy to provision users because the accounts are tied to GitHub.
+  
+
+* **Q:** Does this work with self-signed certs on localhost?
+  
+  **A:** Yes! Apache is intentionally configured to ignore all certificate errors. Traffic sent between the remote web server and the localhost is sent securely over SSH, so there should be no security concerns about using self signed certs here.
+  
+
+* **Q:** A user having trouble setting up the tunnel - how can test using their account?
+  
+  **A:** If the user is named `alligator-lovely`, open `/home/alligator-lovely/.ssh/authorized_kes` and add your public SSH key on a new line.  This way you can SSH in to remove any doubt that the server is working correctly.
+  
+
+* **Q:** I need to add more users after setting this up a first time - can I re-run the script?
+  
+  **A:** Yes, the script is safe to re-run multiple times. Edit the `user.txt` file to only have the new users.
+  
+
+* **Q:** A user changed their SSH key on GitHub - how do I update their account?
+  
+  **A:** Edit the `user.txt` file to only have the one user.  They will lose their original port mapping and get a new one.
+
+
+* **Q:** How do I get a list of all the users in my GH org?
+  
+  **A:** Get a [personal GH token](https://github.com/settings/tokens), then call the [list org members API](https://docs.github.com/en/rest/reference/orgs#list-organization-members) with this call `curl -H "Authorization: token  TOKEN" https://api.github.com/orgs/ORG/members`. Be sure to replace `TOKEN` and `ORG` with your token and your org. There's rate limits, but [at 5000/hr](https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting), they probably don't apply.
+
+
+* **Q:** Is there a rate limit to the number of Let's Encrypt certs I can request?
+  
+  **A:** Yes! It's [50/week](https://letsencrypt.org/docs/rate-limits/).  This script should really be using Subject Alternative Name (SAN) mechanism...hopefully soon!
+  
+
+* **Q:** I added a GH user, but it doesn't create an account for them, why not?
+  
+  **A:** All users must have an SSH key on GH.  Check `https://github.com/USERNAME.keys` and ensure a key is listed there. Re-run the script if need be after a key has been added by the user.
--- installTunnelServer.sh
+++ installTunnelServer.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# todo - put in a single loop?  fewer loops?
 # todo - check for Nth run to not create users that already exist etc
-# todo - save decent looking HTML instructions in $DOMAIN vhost
 # todo - maybe check for ubuntu?
-# todo - consolidate certbot calls with "-d DOMAIN" to reduce API calls?
-# todo - check for GH user exists (http 200) and user having keys before creating account
+# todo - consolidate certbot calls with "-d DOMAIN" via SAN to reduce API calls?
+# todo - cache SSH keys on first validation to avoid subsequent API calls to GH to get keys again
 
 DOMAIN="$1"
 EMAIL="$2"
@@ -45,17 +43,42 @@ fi
 
 # thanks https://stackoverflow.com/a/18216122
 if [ "$EUID" -ne 0 ]; then
-  echo "ERROR - You are not as root"
+  echo "ERROR - You are not root"
   echo ""
   echo "Exiting"
   echo ""
-  exit
+#  exit
 fi
 
 echo ""
-echo "This script assumes you're root and assumes you"
-echo "want to set a bunch of SSH tunnels to reverse proxy"
-echo "HTTPs and HTTP traffic."
+echo " ------ Verifying users who have SSH keys on GH, this may take a while... ------ "
+echo " "
+VALID_USERS=()
+for USER in $(cat user.txt); do
+  keys=$(curl -s /dev/stdout https://github.com/${USER}.keys)
+  if [[ ! -z "$keys" ]]; then
+    VALID_USERS+=("$USER")
+  else
+    echo " - Skipping $USER, no SSH key found"
+  fi
+done
+
+echo " "
+echo "This script assumes:"
+echo " "
+echo " - you're root on Ubuntu"
+echo " - you have a wildcard DNS entry pointed to this server "
+echo "   for *.$DOMAIN"
+echo " - you want to set up a bunch of SSH tunnels to reverse  "
+echo "   proxy HTTPs and HTTP traffic"
+echo " - are on on a machine dedicated to this purpose"
+echo ""
+echo "Will create accounts for these GitHub users using their"
+echo "SSH keys on GitHub:"
+echo ""
+for i in "${VALID_USERS[@]}"; do
+  echo " - $i"
+done
 echo ""
 echo "See https://github.com/mrjones-plip/mrjones-medic-scratch/tree/main/SshTunnelServer "
 echo "for more info"
@@ -65,84 +88,141 @@ echo ""
 read -n1 -s
 
 # install apache, rpl & certbot then enable mods
-# todo - uncomment this - this way to speed testing
 echo ""
-echo "Installing required software..."
+echo " ------ Updating OS and installing required software, this might take a while... ------ "
 echo ""
-apt -q update&&apt -y -q dist-upgrade&&apt -q install -y  apache2  rpl&&systemctl --now enable apache2
+apt -qq update&&apt -y -qqq dist-upgrade&&apt -qqq install -y  apache2  rpl&&systemctl --now enable apache2
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
-a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html
+mv /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/000-default-ssl.conf
+a2ensite 000-default-ssl.conf
 
-# todo validate this works
 echo ""
-echo "Adding users..."
+echo " ------ Adding users... ------ "
 echo ""
 cp ./press_to_exit.sh /bin/press_to_exit.sh
-for i in $(cat user.txt); do
+sleep 1
+for i in "${VALID_USERS[@]}"; do
   useradd -m -d /home/$i -s /bin/press_to_exit.sh $i
 done
 
 echo ""
-echo "Setting MOTD..."
+echo " ------ Setting MOTD... ------ "
 echo ""
 sudo chmod -x /etc/update-motd.d/*
 cp motd /etc/update-motd.d/02-ssh-tunnel-info
 sudo chmod +x /etc/update-motd.d/02-ssh-tunnel-info
 
-# create .ssh dir and authorized_keys with right perms and ownership
-# todo validate this works
-for i in $(cat user.txt); do
-  mkdir /home/$i/.ssh
+echo ""
+echo " ------ Adding SSH keys for users and setting file perms. This may take a while... ------ "
+echo ""
+for i in "${VALID_USERS[@]}"; do
+  mkdir -p /home/$i/.ssh
   touch /home/$i/.ssh/authorized_keys
   chown $i:$i /home/$i/.ssh
   chown $i:$i /home/$i/.ssh/authorized_keys
   chmod 700 /home/$i/.ssh
   chmod 600 /home/$i/.ssh/authorized_keys
-done
-
-# fetch .ssh keys into authorized keys file
-for i in $(cat user.txt); do
   curl -s https://github.com/$i.keys -o /home/$i/.ssh/authorized_keys
 done
 
-# add mrjones key to all to help testing (optional)
-for i in $(cat user.txt); do
-  curl -s https://github.com/mrjones-plip.keys >>/home/$i/.ssh/authorized_keys
-done
-
-#  create one file per user vhost and a custom port per file.
-for i in `cat user.txt`; do
-  cp ./apache.conf /etc/apache2/sites-available/$i.$DOMAIN.conf
-  rpl --encoding UTF-8  -q SUBDOMAIN $i /etc/apache2/sites-available/$i.$DOMAIN.conf
-  rpl --encoding UTF-8  -q DOMAIN $DOMAIN /etc/apache2/sites-available/$i.$DOMAIN.conf
+echo ""
+echo "Adding apache vhost files..."
+echo ""
+for i in "${VALID_USERS[@]}"; do
   rand=`shuf -i1000-5000 -n1`
-  rpl --encoding UTF-8  -q PORT $rand /etc/apache2/sites-available/$i.$DOMAIN.conf
+  FQDNconf="${i}.${DOMAIN}.conf"
+  FQDN_ssl_conf="${i}-ssl.${DOMAIN}.conf"
+
+  cp ./apache.conf /etc/apache2/sites-available/$FQDNconf
+  cp ./apache.ssl.conf /etc/apache2/sites-available/$FQDN_ssl_conf
+
+  rpl -q --encoding UTF-8  -q SUBDOMAIN $i /etc/apache2/sites-available/$FQDNconf
+  rpl -q  --encoding UTF-8  -q SUBDOMAIN $i /etc/apache2/sites-available/$FQDN_ssl_conf
+
+  rpl  -q --encoding UTF-8  -q DOMAIN $DOMAIN /etc/apache2/sites-available/$FQDNconf
+  rpl  -q --encoding UTF-8  -q DOMAIN $DOMAIN /etc/apache2/sites-available/$FQDN_ssl_conf
+
+  rpl  -q --encoding UTF-8  -q PORT $rand /etc/apache2/sites-available/$FQDNconf
+  rpl  -q --encoding UTF-8  -q PORT $rand /etc/apache2/sites-available/$FQDN_ssl_conf
+
+  a2ensite $FQDNconf $FQDN_ssl_conf
 done
 
-
-# enable vhosts
-for i in `cat user.txt`; do
-  a2ensite $i.$DOMAIN.conf
-done
-
-#enable certbot certs , change email
-for i in `cat user.txt`; do
+echo ""
+echo " ------ Fetching certs from Let's Encrypt... ------ "
+echo ""
+for i in "${VALID_USERS[@]}"; do
   sudo certbot  --apache   --non-interactive   --agree-tos   --email $EMAIL --domains $i.$DOMAIN
 done
 
-# reload apache so new config takes effect
+echo ""
+echo " ------ Creating last cert for bare $DOMAIN domain... ------ "
+echo ""
+sudo certbot  --apache   --non-interactive   --agree-tos   --email $EMAIL --domains $DOMAIN
+
+echo ""
+echo " ------ Configuring and Reloading apache... ------ "
+echo ""
+a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html ssl
 systemctl reload apache2
+MAPPING_NOSSL=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|grep -v '\-ssl\.'|cut -d/ -f5,8|cut -d: -f1,3|awk 'BEGIN{FS=OFS=":"}{print $2 FS  $1}'|sed -r 's/.conf//g'|sed -r 's/:/\t/g'| awk '{print "\t" $0}')
+MAPPING_SSL=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*-ssl\.*|cut -d/ -f5,8|cut -d: -f1,3|awk 'BEGIN{FS=OFS=":"}{print $2 FS  $1}'|sed -r 's/.conf//g'|sed -r 's/:/\t/g'| awk '{print "\t" $0}')
+SAMPLE_PORT=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f3|tail -n1)
+SAMPLE_HOST=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f1|sed 's/.conf//g'|tail -n1)
+SAMPLE_LOGIN=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f1|sed 's/.conf//g'|tail -n1|cut -d. -f1)
+
+echo "
+<style>
+* {
+	color: white;
+	background: black;
+}
+</style>
+<pre>
+ports for local <code>http</code> hosts:
+
+$MAPPING_NOSSL
+
+ports for local <code>https</code> hosts:
+
+$MAPPING_SSL
+
+use:
+
+    ssh -T -R PORT-FROM-ABOVE:127.0.0.1:PORT-ON-DEV GH-HANDLE@${DOMAIN}
+
+example:
+
+    expose local port 80 on https://${SAMPLE_HOST} using ${SAMPLE_LOGIN}'s GH keys
+
+    ssh -T -R ${SAMPLE_PORT}:127.0.0.1:80 ${SAMPLE_LOGIN}@${DOMAIN}
+
+note:
+
+    Apache is configured on the http hosts to speak http to your localhost
+    server. Conversely, it is configured to speak https on the https hosts.
+    If you mix these up it will try and speak https to http (or vise versa)
+    and it will fail.
+
+more:
+
+    https://github.com/mrjones-plip/mrjones-medic-scratch/tree/main/SshTunnelServer
+</pre>
+" > /var/www/html/index.html
 
 echo ""
-echo "Outputting mapping..."
+echo " ------ Here's the final mapping for http: ------ "
 echo ""
-echo "-----------------"
-grep '        ProxyPassReverse ' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f1,3
-echo "-----------------"
+echo "${MAPPING_NOSSL}"
+echo ""
+echo " ------ Here's the final mapping for https: ------ "
+echo ""
+echo "${MAPPING_SSL}"
+
+echo ""
+echo "All this was saved to /var/www/html/index.html which is found on https://${DOMAIN}"
 echo ""
 
-
-echo ""
-echo "Done!"
+echo " ------ All done - enjoy! ------ "
 echo ""
